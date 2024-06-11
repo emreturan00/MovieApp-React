120e5328ec911f8960954c70832b8096e2273bfe
emre <turanemre2600@gmail.com>
2024-06-11 12:30:35 +0300

    feat: Add buy ticket functionality and improve styling

--- src/Components/Seats2/Seats2.jsx
+++ src/Components/Seats2/Seats2.jsx
@@ -1,103 +1,85 @@
 import React, { useState } from 'react';
-import { Link } from 'react-router-dom';
-import { useLocation } from 'react-router-dom';
-import { useNavigate } from 'react-router-dom';
-
-
+import { Link, useLocation, useNavigate } from 'react-router-dom';
 
 import './Seats2.css';
 import './styles2.css';
 
 const Seats = ({ ogrenciBiletSayisi, tamBiletSayisi }) => {
-
- 
-
   const location = useLocation();
   const totalPrice = location.state.totalPrice;
-
   const cevap = location.state.cevap;
 
-const occupiedSeats = String(cevap)
-  .split(',')
-  .map(Number)
-  .filter(Boolean);
+  const occupiedSeats = String(cevap)
+    .split(',')
+    .map(Number)
+    .filter(Boolean);
 
   const navigate = useNavigate();
-
-
   const [secilen_koltuklar, setsecilen_koltuklar] = useState([]);
   const [errorMessage, setErrorMessage] = useState(null);
 
   const handleSeatClick = (seatIndex) => {
-  if (occupiedSeats.includes(seatIndex)) {
-    return; // Don't do anything if the seat is occupied
-  }
+    if (occupiedSeats.includes(seatIndex)) {
+      return; // Don't do anything if the seat is occupied
+    }
 
-  setsecilen_koltuklar(prevsecilen_koltuklar => {
-    if (prevsecilen_koltuklar.includes(seatIndex)) {
-      return prevsecilen_koltuklar.filter(seat => seat !== seatIndex);
-    } else {
-      if (prevsecilen_koltuklar.length < ogrenciBiletSayisi + tamBiletSayisi) {
-        return [...prevsecilen_koltuklar, seatIndex];
+    setsecilen_koltuklar((prevsecilen_koltuklar) => {
+      if (prevsecilen_koltuklar.includes(seatIndex)) {
+        return prevsecilen_koltuklar.filter((seat) => seat !== seatIndex);
       } else {
+        if (prevsecilen_koltuklar.length < ogrenciBiletSayisi + tamBiletSayisi) {
+          return [...prevsecilen_koltuklar, seatIndex];
+        } else {
+          setErrorMessage('Cannot select more seats');
+          setTimeout(() => setErrorMessage(null), 2000);
+          return prevsecilen_koltuklar;
+        }
+      }
+    });
+  };
+
+  const handleRowClick = (rowIndex) => {
+    setsecilen_koltuklar((prevsecilen_koltuklar) => {
+      const rowSeats = Array.from({ length: 8 }, (_, i) => i + rowIndex * 8);
+      const newsecilen_koltuklar = prevsecilen_koltuklar.some((seat) => rowSeats.includes(seat))
+        ? prevsecilen_koltuklar.filter((seat) => !rowSeats.includes(seat))
+        : [...prevsecilen_koltuklar, ...rowSeats];
+
+      if (newsecilen_koltuklar.length > ogrenciBiletSayisi + tamBiletSayisi) {
         setErrorMessage('Cannot select more seats');
         setTimeout(() => setErrorMessage(null), 2000);
         return prevsecilen_koltuklar;
+      } else {
+        return newsecilen_koltuklar.sort((a, b) => a - b);
       }
-    }
-  });
-};
+    });
+  };
 
-const handleRowClick = (rowIndex) => {
-  setsecilen_koltuklar(prevsecilen_koltuklar => {
-    const rowSeats = Array.from({ length: 8 }, (_, i) => i + rowIndex * 8);
-    const newsecilen_koltuklar = prevsecilen_koltuklar.some(seat => rowSeats.includes(seat))
-      ? prevsecilen_koltuklar.filter(seat => !rowSeats.includes(seat))
-      : [...prevsecilen_koltuklar, ...rowSeats];
-
-    if (newsecilen_koltuklar.length > ogrenciBiletSayisi + tamBiletSayisi) {
-      setErrorMessage('Cannot select more seats');
-      setTimeout(() => setErrorMessage(null), 2000);
-      return prevsecilen_koltuklar;
-    } else {
-      return newsecilen_koltuklar.sort((a, b) => a - b);
-    }
-  });
-};
   const handleSubmit = async () => {
-
     if (secilen_koltuklar.length !== ogrenciBiletSayisi + tamBiletSayisi) {
-      setErrorMessage('Selected seats do not match the total of the props');
+      setErrorMessage('Selected seats do not match the total of ticket number');
       setTimeout(() => setErrorMessage(null), 2000);
       return;
     }
 
-
-    
     const biletAlData = {
-
-      // secilen_koltuklar
-      // secilenKoltukSayisi: secilen_koltuklar.length,
-      secilenKoltuklar: secilen_koltuklar.join(', ')
-
-      
+      secilenKoltuklar: secilen_koltuklar.join(', '),
     };
 
     try {
       const response = await fetch('http://localhost:8080/api/biletal/save', {
         method: 'PUT',
         headers: {
-          'Content-Type': 'application/json'
+          'Content-Type': 'application/json',
         },
-        body: JSON.stringify(biletAlData)
+        body: JSON.stringify(biletAlData),
       });
 
       if (response.ok) {
-        navigate('/payment2', { state: { totalPrice } });
         const responseData = await response.json();
         console.log('MySQL response:', responseData);
         alert('Seats successfully saved!');
-
+        navigate('/payment2', { state: { totalPrice } });
       } else {
         const errorData = await response.json();
         console.error('Error saving seats:', errorData);
@@ -112,35 +94,31 @@ const handleRowClick = (rowIndex) => {
 
   return (
     <div className="App">
-      {errorMessage && 
-      <div style={{ 
-        color: 'red', 
-        position: 'fixed', 
-        top: '50%', 
-        left: '50%', 
-        transform: 'translate(-50%, -50%)',
-        backgroundColor: 'white',
-        padding: '10px',
-        borderRadius: '5px',
-        zIndex: 1000
-      }}>
-        {errorMessage}
-      </div>
-    }
+      {errorMessage && (
+        <div
+          style={{
+            color: 'red',
+            position: 'fixed',
+            top: '50%',
+            left: '50%',
+            transform: 'translate(-50%, -50%)',
+            backgroundColor: 'white',
+            padding: '10px',
+            borderRadius: '5px',
+            zIndex: 1000,
+          }}
+        >
+          {errorMessage}
+        </div>
+      )}
 
       <div className="nav-buttons">
-                <Link to="/2" className="nav-button">HOME</Link>
-                <Link to="/about-us2" className="nav-button">ABOUT US</Link>
-                <Link to="/our-team2" className="nav-button">OUR TEAM</Link>
-                <Link to="/my-profile" className="nav-button">MY PROFILE</Link>
+        <Link to="/2" className="nav-button">HOME</Link>
+        <Link to="/about-us2" className="nav-button">ABOUT US</Link>
+        <Link to="/our-team2" className="nav-button">OUR TEAM</Link>
+        <Link to="/my-profile" className="nav-button">MY PROFILE</Link>
       </div>
       <div className="seat-area">
-      {/*  <div className="Movies">
-          <select>
-            { Options here }
-          </select>
-        </div> */}
-        
         <ul className="ShowCase">
           <li>{secilen_koltuklar.length > 0 ? secilen_koltuklar.join(', ') : 'N/A'}</li>
           <li>Selected</li>
@@ -151,18 +129,18 @@ const handleRowClick = (rowIndex) => {
           <div className="seats">
             {[...Array(64)].map((_, seatIndex) => (
               <div
-  className={`
-    seat 
-    ${secilen_koltuklar.includes(seatIndex) ? 'selected' : ''}
-    ${occupiedSeats.includes(seatIndex) ? 'occupied' : ''}
-  `}
-  key={seatIndex}
-  onClick={() => {
-    if (!occupiedSeats.includes(seatIndex)) {
-      handleSeatClick(seatIndex);
-    }
-  }}
-></div>
+                className={`
+                  seat 
+                  ${secilen_koltuklar.includes(seatIndex) ? 'selected' : ''}
+                  ${occupiedSeats.includes(seatIndex) ? 'occupied' : ''}
+                `}
+                key={seatIndex}
+                onClick={() => {
+                  if (!occupiedSeats.includes(seatIndex)) {
+                    handleSeatClick(seatIndex);
+                  }
+                }}
+              ></div>
             ))}
           </div>
           <div className="row-buttons">
@@ -178,9 +156,8 @@ const handleRowClick = (rowIndex) => {
         </div>
       </div>
       
-      <Link to="/payment2" className="blue-btn" onClick={handleSubmit}>Go to Payment</Link>
-      </div>
-    
+      <button className="blue-btn" onClick={handleSubmit}>Go to Payment</button>
+    </div>
   );
 };
 
